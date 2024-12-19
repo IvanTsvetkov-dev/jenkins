17a494dd8667698c196f419f206a59f937cf1a4e
IvanTsvetkov-dev <vvvanusha62@mail.ru>
2024-12-19 22:57:57 +0300

    Adding new unit test for inspect resetValue

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,13 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+
+    @Test
+    public void testResetValue(){
+        Aggregator agr = new Aggregator();
+        double val = 15;
+        agr.addValue(15);
+        agr.reset();
+        assertThat(agr.getSum()).isEqualTo(0);
+    }
 }
